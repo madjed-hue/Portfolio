9eb4850a06abd11fe8b3ab61dd671541c785887d
unknown <kentounmax@gmail.com>
2021-05-03 19:39:09 +0000

    initial commit

---
+++ src/components/myNavbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import FATEH from '../../assets/icons/logo.png';
+import "./Navbar.css";
+
+function NavBar() {
+    return (
+        <>
+            <Navbar fixed="top" collapseOnSelect expand="md"  variant="dark" className="animate-navbar nav-theme justify-content-between">
+                <Navbar.Brand href="#home">
+                    <img className="logo" src={FATEH} alt="" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#skills">Skills</Nav.Link>
+                        <Nav.Link href="#experience">Experience</Nav.Link>
+                        <Nav.Link href="#portfolio">Potfolio</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    )
+}
+
+export default NavBar
